44dc5813dbddd5873293abb02704de1986aa5c09
Minho <oiloilo44@gmail.com>
2022-06-16 09:58:35 +0900

    edit 화면에서만 단축키 작동하도록 변경

--- contentScripts/script.js
+++ contentScripts/script.js
@@ -1,42 +1,45 @@
 document.addEventListener('keydown', function (e) {
+    let url = window.location.href
+    let editChk = url.includes('bots/repository/private/taskbots') && url.includes('edit')
+
     e = e || window.event;
     let tmp
     // Run from here
-    if (e.altKey && (e.key === 'r' || e.key === 'R')) {
-        tmp = document.querySelector('div.taskbotlistnode-title--cursor-pivot button').click()
-        tmp = setTimeout(function () {
+    if (e.altKey && (e.key === 'r' || e.key === 'R') && editChk) {
+        document.querySelector('div.taskbotlistnode-title--cursor-pivot button').click()
+        setTimeout(function () {
             document.querySelector('div.popup--show button[name=action-node-run-from]').click()
         }, 100)
 
         // Expand/Collapse All
-    } else if (e.altKey && (e.key === 'a' || e.key === 'A')) {
+    } else if (e.altKey && (e.key === 'a' || e.key === 'A') && editChk) {
         let css = 'div[class=taskbotlistnode-collapser]'
         if (document.querySelector(css) == null)
             css = 'div.taskbotlistnode-collapser--collapsed'
 
         while (true) {
-            var collapser = document.querySelector(css)
+            let collapser = document.querySelector(css)
             if (collapser == null)
                 break
             collapser.click()
         }
 
         // Go to First node
-    } else if (e.altKey && (e.key === 'w' || e.key === 'W')) {
-        tmp = document.querySelector('div.taskbotlistnode-title--cursor').closest('div.taskbotlistnode--mode-edit').parentElement.querySelector('div.taskbotlistnode-title').click()
+    } else if (e.altKey && (e.key === 'w' || e.key === 'W') && editChk) {
+        document.querySelector('div.taskbotlistnode-title--cursor').closest('div.taskbotlistnode--mode-edit').parentElement.querySelector('div.taskbotlistnode-title').click()
 
         // Go to Last node
-    } else if (e.altKey && (e.key === 's' || e.key === 'S')) {
+    } else if (e.altKey && (e.key === 's' || e.key === 'S') && editChk) {
         let lastNode = document.querySelector('div.taskbotlistnode-title--cursor').closest('div.taskbotlistnode--mode-edit').parentElement.lastChild.querySelector('div.taskbotlistnode-title')
         if (lastNode == null)
             lastNode = document.querySelector('div.taskbotlistnode-title--cursor').closest('div.taskbotlistnode--mode-edit').parentElement.lastChild.previousSibling.querySelector('div.taskbotlistnode-title')
         lastNode.click()
 
         // Uncheck all properties
-    } else if (e.altKey && (e.key === 'q' || e.key === 'Q')) {
+    } else if (e.altKey && (e.key === 'q' || e.key === 'Q') && editChk) {
         document.querySelector("div.editordetails span.fa-caret-right").click()
         tmp = document.querySelectorAll("div.editordetails div.taskbotpropertiesfield div[aria-checked='true']")
-        for (var check of tmp) {
+        for (let check of tmp) {
             check.click()
         }
     }
